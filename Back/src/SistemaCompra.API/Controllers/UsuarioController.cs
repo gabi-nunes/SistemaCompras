b2e18f5e1ec42bad65ddd692718224a1dd06a8e8
Gabi Nunes <gabiedununes@hotmail.com>
2021-03-07 17:19:03 -0300

    First commit

---
+++ Back/src/SistemaCompra.API/Controllers/UsuarioController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SistemaCompra.API.Model;
+
+namespace SistemaCompra.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsuarioController : ControllerBase
+    {
+      
+
+        public UsuarioController()
+        {
+            
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "ValueTask";
+        }
+
+         [HttpGet("{id}")]
+        public string GetById(int id)
+        {
+            return "ValueTask";
+        }
+
+
+    }
+}
---
+++ Back/src/SistemaCompra.API/Model/Usuario.cs
@@ -0,0 +1,13 @@
+namespace SistemaCompra.API.Model
+{
+    public class Usuario
+    {
+        public int UsuarioId { get; set; }
+
+        public string NomeUsuario { get; set; }
+
+        public string  SenhaUsuario { get; set; }
+
+        public string CargoUsuario { get; set; }
+    }
+}
